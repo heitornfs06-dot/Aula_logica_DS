91e83f076c9f18d1c110217f569ba82181a81c0d
Vignotto <heitornfs06@gmail.com>
2026-09-17 21:48:59 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-
-int main()
-{
-    char nome;
-    int idade;
-    int nascimento;
-    int maioridade;
-
-
-
-    printf("Ola, qual seu nome??\n");
-    scanf("%s", nome);
-    printf("Qual sua idade?\n");
-    scanf("%d", &idade);
-    printf("Qual seu ano de nascimento?\n");
-    scanf("%d", &nascimento);
-
-    maioridade = idade - nascimento;
-
+int main() {
+    printf("Hello, World!\n");
     return 0;
 }
